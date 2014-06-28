e0b757ff87b7cb2ff0749f21cfe580e8d24aaee0
nicholasxjy <yitaqi2011@gmail.com>
2014-06-28 17:10:54 +0800

    modal modify

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,6 +4,9 @@
 $(document).ready(function() {
     //tooltip init
     $('.nav-tooltip').tooltip();
+
+    //popover
+    $('.info-popover').popover();
     $('#btn-signin').click(function(event) {
         var $form = $('#signin-wrap');
         var name = $form.find('input[name="name"]').val();
@@ -83,9 +86,36 @@ $(document).ready(function() {
       var time = $parent.data('posttime');
       var content = $parent.data('postcontent') || '';
 
-      var modal = '<div class="modal fade" id="comment-post-modal" tabindex="-1" role="dialog" aria-labelledby="myPostModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button><h4 class="modal-title" id="myModalLabel">'+ '回复@'+ username +'</h4></div><div class="modal-body"><div class="comment-item"><div class="comment-item-inner-left"><img class="img-rounded img-responsive nav-tooltip" src="'+ avatar +'" data-toggle="tooltip" title="' + username + '" width="32" height="32"></div><div class="comment-item-inner-right"><div class="author-information"><a href="/u/' + username + '"><strong class="fullname">' + username + '</strong></a><small class="create-time"><a class="nav-tooltip" href="#">' + time + '</a></small></div><p>' + content + '</p></div></div></div><div class="modal-footer"><button id="modal-post-btn" type="button" class="btn btn-primary btn-post-comment"><i class="fa fa-pencil-square-o"></i></button></div></div></div></div>';
+      var modal = '<div class="modal fade" id="comment-post-modal" tabindex="-1" role="dialog" aria-labelledby="myPostModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button><h4 class="modal-title" id="myModalLabel">'+ '回复@'+ username +'</h4></div><div class="modal-body"><div class="modal-comment-item"><div class="modal-comment-inner-left"><img class="img-rounded img-responsive nav-tooltip" src="'+ avatar +'" title="' + username + '" width="48" height="48"></div><div class="modal-comment-inner-right"><div class="author-information"><a href="/u/' + username + '"><strong class="fullname">' + username + '</strong></a><small class="create-time"><a class="nav-tooltip" href="#">' + time + '</a></small></div><p>' + content + '</p></div></div><div class="comment-input-area"><textarea class="share-words" row="3">' + '@' + username + ' ' + '</textarea></div></div><div class="modal-footer"><button id="modal-post-btn" type="button" class="btn btn-primary btn-post-comment"><i class="fa fa-pencil-square-o"></i></button></div></div></div></div>';
       $('#comment-post-modal').remove();
       $('.wrap').append(modal);
       $('#comment-post-modal').modal();
+    });
+    //init dropzone
+    $('div.dropzone').dropzone({
+      url: '/create',
+      paramName: 'file',
+      maxFilesize: 10,
+      parallelUploads: 10,
+      acceptedFiles: 'image/*',
+      addRemoveLinks: true,
+      autoProcessQueue: false,
+      uploadMultiple: true,
+      init: function() {
+
+      }
+    });
+    //attach-photo-link click
+    $('.attach-photo-link').on('click', function() {
+      $('#link-wrap').hide();
+      $('#dropzone-wrap').show();
+    });
+    $('.attach-link-link').on('click', function() {
+      $('#dropzone-wrap').hide();
+      $('#link-wrap').show();
+    });
+
+    $('.attach-link-cancel').on('click', function() {
+      $('.attach-link-input').val('');
     });
 });
